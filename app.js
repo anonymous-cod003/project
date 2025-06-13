f96b18b606434c47db07f0c22977923888893e6b
anonymous-cod003 <arun.arora0045@gmail.com>
2025-06-13 17:07:38 +0530

    new javascript file

---
+++ app.js
@@ -0,0 +1,26 @@
+// let inp = document.querySelector("input");
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+
+btn.addEventListener("click", () => {
+    let inp = document.querySelector("input");
+
+    if (inp.value == '') {
+        alert("Field cannot be empty");
+    }
+    else {
+        let deltbn = document.createElement("button");
+        deltbn.innerText = "Delete Task";
+        let li = document.createElement("li");
+        li.innerText = inp.value;
+        ul.appendChild(li);
+        li.appendChild(deltbn);
+        inp.value = '';
+
+         deltbn.addEventListener("click" ,() => {
+   
+          ul.removeChild(li);     
+    } )
+    }
+   
+});
